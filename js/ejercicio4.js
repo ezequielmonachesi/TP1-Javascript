3ef875f20c7c0085568e1e536f12904f1858f34f
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-02-25 08:16:19 -0300

    modificando su nombre de ususario a Hola usuario

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -1,2 +1,2 @@
 let nombreUsuario = prompt('Ingrese el nombre de usuario que va a utilizar');
-document.write('Su nombre de usuario es: ' + nombreUsuario + '.');
+document.write('Hola ' + nombreUsuario + '.');
