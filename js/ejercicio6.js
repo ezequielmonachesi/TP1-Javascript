f79f4d6fdd458fbaec55091b5e11075521b80336
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-02-28 09:45:46 -0300

    ejercicio6 finalizado

---
+++ js/ejercicio6.js
@@ -0,0 +1,8 @@
+let n1 = parseInt(prompt('Ingrese el primer número'));
+let n2 = parseInt(prompt('Ingrese el segundo número'));
+
+if(n1 >= n2){
+    document.write(`El número mayor es el ${n1}`);
+}else{
+    document.write(`El número mayor es el ${n2}`)
+}
